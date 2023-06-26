81e7f7a52771de01f01b8821ff40580f9554f949
AlexMagar <lx.mgr07@gmail.com>
2023-06-26 23:06:57 +1000

    added Home, Signup, and Signin components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 
-import './App.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import {Routes, Route} from 'react-router-dom';
-import { Signin } from './components/pages/singnup-signin/Signin';
-import { Signup } from './components/pages/singnup-signin/Signup';
-import Home from './components/pages/home/Home';
+import "./App.css"
+import "react-toastify/dist/ReactToastify.css"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Signup from "./pages/signup-signin/Signup";
+import Singnin from "./pages/signup-signin/Singnin";
+
 
 function App() {
   return (
     <div className="">
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/new-admin' element={<Signup />}/>
-        <Route path='/login' element={<Signin />}/>
+        <Route path="/" element={ <Home />} />
+        <Route path="/new-admin" element={ <Signup />} />
+        <Route path="/login" element={ <Singnin />} />
       </Routes>
-      <ToastContainer/>
     </div>
   );
 }
